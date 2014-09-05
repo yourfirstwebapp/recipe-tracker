c039c697beccea0723bfeec84baec41d66317b30
Alex Coleman <yourfirstwebapp@alexpcoleman.com>
2014-09-05 15:49:51 -0500

    added shim

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -3,6 +3,10 @@
 <head>
   <meta charset="utf-8">
   <title>@yield('title') | Recipe Tracker | Your First Web App</title>
+  <!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js">
+  </script>
+  <![endif]-->
 </head>
 <body>
 

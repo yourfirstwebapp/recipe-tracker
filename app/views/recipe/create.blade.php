0f445fb80b8ede0458f1a4ec97efce5159e01850
Alex Coleman <yourfirstwebapp@alexpcoleman.com>
2014-09-25 11:22:42 -0500

    fixed create inputs

--- app/views/recipe/create.blade.php
+++ app/views/recipe/create.blade.php
@@ -15,23 +15,23 @@
 
     <div class="form-group">
       {{ Form::label('author_id', 'Author') }}
-      {{ Form::select('author_id', $authors,
+      {{ Form::select('author_id', $authors, '',
         array('class' => 'form-control')) }}
     </div>
 
     <div class="form-group">
       {{ Form::label('servings', 'Servings') }}
-      {{ Form::selectRange('servings', 1, 100,
+      {{ Form::selectRange('servings', 1, 100, '',
         array('class' => 'form-control')) }}
     </div>
 
     <div class="form-group form-time clearfix">
       {{ Form::label('time_prep', 'Prep Time') }}
-      {{ Form::selectRange('time_prep_hours', 0, 24,
+      {{ Form::selectRange('time_prep_hours', 0, 24, '',
         array('class' => 'form-control')) }}
       {{ Form::label('time_prep_hours', 'hrs',
         [ 'class' => 'select-label' ]) }}
-      {{ Form::selectRange('time_prep_minutes', 0, 60,
+      {{ Form::selectRange('time_prep_minutes', 0, 60, '',
         array('class' => 'form-control')) }}
       {{ Form::label('time_prep_minutes', 'mins',
         [ 'class' => 'select-label' ]) }}
@@ -39,11 +39,11 @@
 
     <div class="form-group form-time clearfix">
       {{ Form::label('time_cook', 'Cook Time') }}
-      {{ Form::selectRange('time_cook_hours', 0, 24,
+      {{ Form::selectRange('time_cook_hours', 0, 24, '',
         array('class' => 'form-control')) }}
       {{ Form::label('time_cook_hours', 'hrs',
         array('class' => 'select-label')) }}
-      {{ Form::selectRange('time_cook_minutes', 0, 60,
+      {{ Form::selectRange('time_cook_minutes', 0, 60, '',
         array('class' => 'form-control')) }}
       {{ Form::label('time_cook_minutes', 'mins',
         array('class' => 'select-label')) }}
